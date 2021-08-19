5d20b7c285e07da9cb345c9d7fcaccb81d8fa1a5
haneulch <khaneulc@kakao.com>
2021-08-19 14:46:25 +0900

    fix : change swagger config

--- auth/src/main/java/com/mybury/bucketlist/auth/config/SwaggerConfig.java
+++ auth/src/main/java/com/mybury/bucketlist/auth/config/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package com.mybury.bucketlist.auth.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,13 +29,24 @@ public class SwaggerConfig {
 	
 	@Bean
 	public Docket api() {
+		ParameterBuilder builder = new ParameterBuilder();
+		builder.name("X-Auth-Token")
+				.description("X-Auth-Token")
+				.modelRef(new ModelRef("string"))
+				.parameterType("header")
+				.required(false)
+				.build();
+		
+		List<Parameter> builders = new ArrayList<Parameter>();
+		builders.add(builder.build());
+		
 		return new Docket(DocumentationType.SWAGGER_2)
+						.globalOperationParameters(builders)
 						.useDefaultResponseMessages(false)
 						.apiInfo(this.apiInfo())
 						.select()
 			            .apis(RequestHandlerSelectors.basePackage("com.mybury.bucketlist.auth.v2.controller"))
 			            .paths(PathSelectors.any())
 						.build();
-	}
-	
+	}	
 }
