ed5a4cd1739c04b0bf61b4fa7efd8fe3d5c1a582
haneulch <khaneulc@kakao.com>
2021-08-21 11:27:12 +0900

    fix : organize unused imports

--- core/src/main/java/com/mybury/bucketlist/core/v2/service/FollowServiceImpl.java
+++ core/src/main/java/com/mybury/bucketlist/core/v2/service/FollowServiceImpl.java
@@ -1,17 +1,13 @@
 package com.mybury.bucketlist.core.v2.service;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.mybury.bucketlist.core.domain.Follow;
 import com.mybury.bucketlist.core.domain.User;
-import com.mybury.bucketlist.core.domain.UserSummary;
 import com.mybury.bucketlist.core.repository.UserRepository;
 import com.mybury.bucketlist.core.v2.repository.FollowRepository;
 import com.mybury.bucketlist.core.v2.vo.FollowRequest;
