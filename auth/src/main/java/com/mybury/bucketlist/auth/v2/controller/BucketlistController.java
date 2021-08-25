636e96add5bc43e42b466329dde1a4daa2d8f582
haneulch <khaneulc@kakao.com>
2021-08-25 18:49:35 +0900

    feat : dday별 bucketlist API 추가

---
+++ auth/src/main/java/com/mybury/bucketlist/auth/v2/controller/BucketlistController.java
@@ -0,0 +1,33 @@
+package com.mybury.bucketlist.auth.v2.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mybury.bucketlist.auth.annotation.AccessTokenCheck;
+import com.mybury.bucketlist.core.service.BucketlistManager;
+import com.mybury.bucketlist.core.util.ResponseUtils;
+import com.mybury.bucketlist.core.v2.vo.UserRequest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "Bucketlist", tags = {"버킷리스트"})
+@RestController
+@RequestMapping("/v2/bucketlist")
+public class BucketlistController {
+	private final BucketlistManager bucketlistManager;
+	
+	BucketlistController(BucketlistManager bucketlistManager) {
+		this.bucketlistManager = bucketlistManager;
+	}
+	
+	@AccessTokenCheck
+	@ApiOperation(value = "dday별 버킷리스트")
+	@PostMapping("/dday")
+	public ResponseEntity<Object> dday(@RequestBody UserRequest request) {
+		return ResponseUtils.success(bucketlistManager.findBydDateIsNotNullAndUser_Id(request.getUserId()));
+	}
+}
