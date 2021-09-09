66a41738493bbe60765ffd0f14f7e746d084e7cf
haneulch <khaneulc@kakao.com>
2021-09-09 15:36:28 +0900

    feat : add new constants

---
+++ auth/src/test/java/com/mybury/bucketlist/auth/TestInfoContants.java
@@ -0,0 +1,10 @@
+package com.mybury.bucketlist.auth;
+
+public class TestInfoContants {
+	private TestInfoContants() {}
+	
+	public static String TEST_USER_ID = "2c94b0817b348920017b348a09d70001";
+	public static String TEST_EMAIL = "test11@gmail.com";
+	public static String TEST_CATEGORY_ID = "2c94b0817b33eca6017b33ecb14d0015";
+	public static String TEST_CATEGORY_NAME = "test";
+}
