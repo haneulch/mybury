ceab2a33fcc0fdaaf9cebc5a05318c75cd876c55
haneulch <khaneulc@kakao.com>
2021-09-11 17:02:09 +0900

    fix : init test

--- auth/src/test/java/com/mybury/bucketlist/auth/SettingControllerTest.java
+++ auth/src/test/java/com/mybury/bucketlist/auth/SettingControllerTest.java
@@ -1,5 +1,52 @@
 package com.mybury.bucketlist.auth;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mybury.bucketlist.core.util.JwtUtils;
+
 public class SettingControllerTest {
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private MockMvc mockMvc;
+	
+	@Rule
+	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+	            .apply(documentationConfiguration(this.restDocumentation))
+				.alwaysDo(document("{method-name}",
+						preprocessRequest(
+								modifyUris()
+									.scheme("http")
+									.host("www.my-bury.com").removePort(),
+								prettyPrint()
+						),
+						preprocessResponse(prettyPrint())))
+				.build();
+	}
 
 }
