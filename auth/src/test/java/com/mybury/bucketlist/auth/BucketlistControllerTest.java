1b0108fff7eac1a743b43b384fec0698eec7de59
haneulch <khaneulc@kakao.com>
2021-09-12 17:34:23 +0900

    feat : add new Test Class

---
+++ auth/src/test/java/com/mybury/bucketlist/auth/BucketlistControllerTest.java
@@ -0,0 +1,11 @@
+package com.mybury.bucketlist.auth;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BucketlistControllerTest {
+
+}
