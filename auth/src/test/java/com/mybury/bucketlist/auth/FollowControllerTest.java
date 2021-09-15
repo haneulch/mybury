cb3cdd953eef5cf5ce58a294fa93f926d9ec8946
haneulch <khaneulc@kakao.com>
2021-09-15 17:37:54 +0900

    fix : init follow test class

--- auth/src/test/java/com/mybury/bucketlist/auth/FollowControllerTest.java
+++ auth/src/test/java/com/mybury/bucketlist/auth/FollowControllerTest.java
@@ -1,5 +1,56 @@
 package com.mybury.bucketlist.auth;
 
-public class FollowControllerTest {
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mybury.bucketlist.core.util.JwtUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FollowControllerTest {
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private MockMvc mockMvc;
+	
+	@Rule
+	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+	            .apply(documentationConfiguration(this.restDocumentation))
+				.alwaysDo(document("{method-name}",
+						preprocessRequest(
+								modifyUris()
+									.scheme("http")
+									.host("www.my-bury.com").removePort(),
+								prettyPrint()
+						),
+						preprocessResponse(prettyPrint())))
+				.build();
+	}
 }
